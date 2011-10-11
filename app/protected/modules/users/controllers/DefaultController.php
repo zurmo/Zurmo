4acfc9873745bb49a7de77dd3f9c00331852959d
digimagic <none@none>
2011-10-10 23:59:07 -0500

    Fixing issue with user not being able to see his/her profile if they do not have access to the users module.

--- app/protected/modules/users/controllers/DefaultController.php
+++ app/protected/modules/users/controllers/DefaultController.php
@@ -39,7 +39,7 @@
             $filters = array();
             $filters[] = array(
                     ZurmoBaseController::RIGHTS_FILTER_PATH .
-                    ' - modalList, autoComplete, details, edit, changePassword, configurationEdit, securityDetails',
+                    ' - modalList, autoComplete, details, profile, edit, changePassword, configurationEdit, securityDetails',
                     'moduleClassName' => 'UsersModule',
                     'rightName' => UsersModule::getAccessRight(),
             );
@@ -343,7 +343,7 @@
                         UserConfigurationFormAdapter::setConfigurationFromFormForCurrentUser($configurationForm);
                     }
                     Yii::app()->user->setFlash('notification',
-                        yii::t('Default', 'User configuration saved successfully.')
+                        Yii::t('Default', 'User configuration saved successfully.')
                     );
                     $this->redirect(array($this->getId() . '/index'));
                 }
