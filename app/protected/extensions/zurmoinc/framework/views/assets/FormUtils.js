5580085dff5db5b63ee0642c27c1d10bf852d6dd
Nilesh Patkar <bitbucket@antechnologies.co.in>
2011-10-14 15:14:33 +0530

    This commit introduces the unminify version of FormUtils.js so as to improve the redability of the function. This commit is associated with the issue "When clicking 'advanced search', the owner only checkbox is not working in search." that was resolved in the previous commit. This issue can be referred on PivotalTracker https://www.pivotaltracker.com/story/show/19203953
    --HG--
    branch : additionalWalkthroughTests

--- app/protected/extensions/zurmoinc/framework/views/assets/FormUtils.js
+++ app/protected/extensions/zurmoinc/framework/views/assets/FormUtils.js
@@ -4,7 +4,30 @@
  * Report Bugs: <bugs@truthanduntruth.com>
  * Copyright 2010
  */
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(1($){$.n.6=1(c){2 d={3:\'3\',4:\'o\',7:"8[0!=\'p\'][0!=\'q\'][0!=\'r\'][0!=\'s\'][0!=9], t, u",j:"8[0=9]",5:{},k:1(){}};2 f={};2 g=$.v(f,d,c);2 h=g.4.w(" ");2 i=\'\';$.x(h,1(a,b){i+=b+\'.6 \'});$(y).4(i,1(e){$(g.7,g.3).z(\'\').A(\'B:C-D\').l(\'m\',\'m\');$(g.j,g.3).l(\'E\',F);g.k()}).5(g.5)}})(G);',43,43,'type|function|var|form|bind|css|clearform|clear|input|checkbox||||||||||clearCheckbox|complete|attr|selected|fn|click|submit|button|hidden|reset|textarea|select|extend|split|each|this|val|find|option|first|child|checked|false|jQuery'.split('|'),0,{}));
+;(function ($) {
+    $.fn.clearform = function (c) {
+        var d = {
+            form: 'form',
+            bind: 'click',
+            clear: "input[type!='submit'][type!='button'][type!='hidden'][type!='reset'][type!=checkbox], textarea, select",
+            clearCheckbox: "input[type=checkbox]",
+            css: {},
+            complete: function () {}
+        };
+        var f = {};
+        var g = $.extend(f, d, c);
+        var h = g.bind.split(" ");
+        var i = '';
+        $.each(h, function (a, b) {
+            i += b + '.clearform '
+        });
+        $(this).bind(i, function (e) {
+            $(g.clear, g.form).val('').find('option:first-child').attr('selected', 'selected');
+            $(g.clearCheckbox, g.form).attr('checked', false);
+            g.complete()
+        }).css(g.css)
+    }
+})(jQuery);
 
 function afterValidateAjaxAction(form, data, hasError)
 {
