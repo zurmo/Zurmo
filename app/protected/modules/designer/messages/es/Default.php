35b4fe395897fcba25fd6caacc44f61c2eca749f
zurmo <none@none>
2011-10-17 23:15:10 -0500

    more work on spanish language.
    --HG--
    branch : Spanish

--- app/protected/modules/designer/messages/es/Default.php
+++ app/protected/modules/designer/messages/es/Default.php
@@ -30,78 +30,78 @@
     // DON'T MAKE A MESS!
     return array(
         'A field with this name is already used.'
-            => '??',
+            => 'Un campo con este nombre ya se utiliza',
         'All panels visible'
-            => '??',
+            => 'Todos los paneles visibles',
         'Create Field'
-            => '??',
+            => 'Crear campo',
         'Designer'
-            => 'DeseÃ±ador',
+            => 'Diseñador',
         'Detail And Edit View'
-            => '??',
+            => 'Vista de formularios',
         'Details View'
-            => '??',
+            => 'Vista de detalles',
         'Display Name'
-            => '??',
+            => 'Etiqueta del campo',
         'Each item must be uniquely named'
-            => '??',
+            => 'Cada elemento debe tener un nombre exclusivo',
         'Edit View'
-            => '??',
+            => 'Formulario de inscripción',
         'Field Name'
-            => '??',
+            => 'Nombre de campo',
         'Field Type'
-            => '??',
+            => 'Tipo de campo',
         'Fields'
-            => '??',
+            => 'Campos',
         'First panel visible, then `show more` link'
-            => '??',
+            => 'Primer panel visible, después `mostrar más` link',
         'General'
-            => '??',
+            => 'General',
         'Invalid panel configuration type'
-            => '??',
+            => 'Tipo de configuración de panel inválido',
         'Layouts'
-            => '??',
+            => 'Presentaciones de página',
         'List View'
-            => '??',
+            => 'Vista de lista',
         'Manage module fields, layouts, and labels.'
-            => '??',
+            => 'Administrar campos de módulo, diseños y etiquetas',
         'Mass Edit View'
-            => '??',
+            => 'Vista de modificación de masa',
         'Maximum Length'
-            => '??',
+            => 'Longitud máxima',
         'Maximum Value'
-            => '??',
+            => 'Valor máximo',
         'Menu'
-            => '??',
+            => 'menú',
         'Menus'
-            => '??',
+            => 'menús',
         'Minimum Value'
-            => '??',
+            => 'valor mínimo',
         'Module'
-            => '??',
+            => 'Módulo',
         'Panels are tabbed'
-            => '??',
+            => 'Los paneles son pestañas',
         'Panels Configuration'
-            => '??',
+            => 'Configuración de paneles',
         'Pick List Values'
-            => '??',
+            => 'Valores de pick list',
         'Popup List View'
-            => '??',
+            => 'Vista de lista popup',
         'Popup Search View'
-            => '??',
+            => 'Búsquada de lista popup',
         'Precision'
-            => '??',
+            => 'precisión',
         'Related List View'
-            => '??',
+            => 'Vista de lista relacionada',
         'Required Field'
-            => '??',
+            => 'Campo requerido',
         'Save Layout'
-            => '??',
+            => 'Publicar',
         'Search View'
-            => '??',
+            => 'Vista de búsqueda',
         'There was an error processing your request.'
-            => '??',
+            => 'Hubo un error al procesar su pedido',
         'Track Audit Log'
-            => '??',
+            => 'Auditar este campo',
     );
 ?>
