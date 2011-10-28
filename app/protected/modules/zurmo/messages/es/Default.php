b65e676dfdbf590ddbe03af17861bab50eb65e9d
zurmo <jason@zurmo.com>
2011-10-28 10:47:32 -0500

    spanish language work
    --HG--
    branch : Spanish

--- app/protected/modules/zurmo/messages/es/Default.php
+++ app/protected/modules/zurmo/messages/es/Default.php
@@ -114,345 +114,345 @@
         'Cancel'
             => 'Cancelar',
         'Changes to active currencies changed successfully.'
-            => '??',
+            => 'Divisas activas cambiado con éxito',
         'Changes to active languages changed successfully.'
-            => '??',
+            => 'Idiomas activos cambiado con éxito',
         'Chart Type'
-            => '??',
+            => 'Tipo de gráfico',
         'City'
-            => '??',
+            => 'Ciudad',
         'Code'
-            => '??',
+            => 'Código',
         'Configuration'
-            => '??',
+            => 'Configuración',
         'Convert'
-            => '??',
+            => 'Convertir',
         'Copyright &#169; Zurmo Inc., 2011. All Rights reserved.'
             => 'Copyright &#169; Zurmo Inc. de 2011. Todos los derechos reservados.',
         'Country'
-            => '??',
+            => 'Pais',
         'Create Group'
-            => '??',
+            => 'Crear grupo',
         'Create Groups'
-            => '??',
+            => 'Crear grupos',
         'Create New'
-            => '??',
+            => 'Crear nuevo',
         'Create Roles'
-            => '??',
+            => 'Crear roles',
         'Currencies: Create'
-            => '??',
+            => 'Divisas: crear',
         'Currencies: List'
-            => '??',
+            => 'Divisas: listar',
         'Currency'
-            => '??',
+            => 'Divisa',
         'Currency Configuration'
-            => '??',
+            => 'Configuración de divisas',
         'Currency in use.'
-            => '??',
+            => 'Divisa en uso',
         'Currency Value'
-            => '??',
+            => 'Valor de divisa',
         'Dashboard portlet list page size'
-            => '??',
+            => 'Cantidad de elementos que figuran en el portlet',
         'Data'
-            => '??',
+            => 'Datos',
         'days'
-            => '??',
+            => 'Días',
         'Delete Group'
-            => '??',
+            => 'Eliminar grupo',
         'Delete Groups'
-            => '??',
+            => 'Eliminar grupos',
         'Delete Role'
-            => '??',
+            => 'Eliminar role',
         'Delete Roles'
-            => '??',
+            => 'Eliminar roles',
         'Deny'
             => 'Denegar',
         'Department'
-            => '??',
+            => 'Departamento',
         'Description'
-            => '??',
+            => 'Descripción',
         'Email Address'
-            => '??',
+            => 'Dirección de email',
         'every'
-            => '??',
+            => 'cada',
         'Everyone'
             => 'Todo el Mundo',
         'Fields with'
             => 'Los campos con',
         'File Content'
-            => '??',
+            => 'Contenido de archivo',
         'File failed to upload. The file is empty.'
-            => '??',
+            => 'Archivo no se pudo cargar. El archivo está vacío.',
         'Files'
-            => '??',
+            => 'Archivos',
         'Filter By Created User'
-            => '??',
+            => 'Filtrar la lista por Creado por',
         'Filtered Lists'
-            => '??',
+            => 'Listas filtradas',
         'First Name'
-            => '??',
+            => 'Primer nombre',
         'Global Configuration'
-            => '??',
+            => 'Configuración global',
         'Global configuration saved successfully.'
-            => '??',
+            => 'Configuración global guardado con éxito',
         'Go to Search'
-            => '??',
+            => 'Ir a la búsqueda',
         'Group'
-            => '??',
+            => 'Grupo',
         'Group Module Permissions'
-            => '??',
+            => 'Permisos',
         'Group Policies'
-            => '??',
+            => 'Reglas de Seguridad',
         'Group Rights'
-            => '??',
+            => 'Derechos de grupo',
         'Groups'
-            => '??',
+            => 'Grupos',
         'here'
-            => 'aquÃ­',
+            => 'aquí',
         'Home'
-            => '??',
+            => 'Inicio',
         'Incorrect username or password.'
-            => '??',
+            => 'Nombre de usuario o contraseña incorrecta',
         'Industry'
-            => '??',
+            => 'industria',
         'Inherited - All'
-            => '??',
+            => 'Heredar - Todos',
         'Inherited - Allow'
-            => '??',
+            => 'Heredar - Permitir',
         'Inherited - Deny'
-            => '??',
+            => 'Heredar - Denegar',
         'Inherited - No'
-            => '??',
+            => 'Heredar - No',
         'Inherited - Owner Only'
-            => '??',
+            => 'Heredar - propietario sólo',
         'Inherited - Yes'
-            => '??',
+            => 'Heredar - Sí',
         'Inherited Value:'
-            => '??',
+            => 'Valor heredado:',
         'Invalid currency code'
-            => '??',
+            => 'Código de divisa inválido',
         'Is Invalid'
-            => '??',
+            => 'Es inválido',
         'Job Title'
-            => '??',
+            => 'Título',
         'Label must be all lowercase.'
-            => '??',
+            => 'Etiqueta debe ser todo minúsculo.',
         'Label must not be empty.'
-            => '??',
+            => 'Etiqueta no debe ser vacío',
         'Label must not contain any special characters.'
-            => '??',
+            => 'La etiqueta no debe contener caracteres especiales',
         'Language'
-            => '??',
+            => 'Lengua',
         'Languages: List'
-            => '??',
+            => 'Idiomas: Lista',
         'Last Name'
-            => '??',
+            => 'Apellido',
         'Last Updated'
-            => '??',
+            => 'Última actualización',
         'List page size'
-            => '??',
+            => 'Número de elementos en la vista de lista',
         'Load'
-            => '??',
+            => 'Cargar',
         'Login'
             => 'Acceder',
         'Logout'
             => 'Salir',
         'Manage Currency Configuration'
-            => '??',
+            => 'Administrar la configuración de divisas',
         'Manage Global Configuration'
-            => '??',
+            => 'Administrar la configuración global',
         'Manage Groups'
-            => '??',
+            => 'Administrar grupos',
         'Manage Roles'
-            => '??',
+            => 'Administrar roles',
         'Member'
-            => '??',
+            => 'Miembro',
         'Member Of'
-            => '??',
+            => 'Miembro de',
         'Members'
-            => '??',
+            => 'Miembros',
         'Missing Required Attributes'
-            => 'Les champs obligatoires doivent être placés dans la vue',
+            => 'Los campos obligatorios se colocará en el punto de vista',
         'Mobile Phone'
-            => '??',
+            => 'Tel. móvil',
         'Module Name - Plural'
-            => '??',
+            => 'Nombre del módulo - Plural',
         'Module Name - Plural (lowercase)'
-            => '??',
+            => 'Nombre del módulo - Plural (en minúsculas)',
         'Module Name - Singular'
-            => '??',
+            => 'Nombre del módulo - Singular',
         'Module Name - Singular (lowercase)'
-            => '??',
+            => 'Nombre del módulo - Singular (en minúsculas)',
         'Module Permissions'
-            => '??',
+            => 'Permisos de módulo',
         'Module Permissions Saved Successfully.'
-            => '??',
+            => 'Permisos de módulo guardado correctamente',
         'My List Configuration View'
-            => '??',
+            => 'Mi vista de lista configuración',
         'Never'
-            => '??',
+            => 'Nunca',
         'No Results Found'
-            => '??',
+            => 'No se encontraron resultados',
         'Non Members'
-            => '??',
+            => 'No miembres',
         'Not Set'
-            => '??',
+            => 'Predeterminado',
         'Not Set - Ad-hoc / Owner'
-            => '??',
+            => 'Predeterminado - Ad-hoc / Propietario',
         'Office Fax'
-            => 'Fax de la Oficina',
+            => 'Fax de Oficina',
         'Office Phone'
-            => 'TelÃ©fono de la Oficina',
+            => 'Tel. de Oficina',
         'Oh no!'
             => 'Oh no!',
         'Only Items I Own'
-            => '??',
+            => 'Sólo mis elementos',
         'Opt Out'
-            => '??',
+            => 'Opt out',
         'Owner'
-            => '??',
+            => 'Propietario',
         'Owner and users in'
-            => '??',
+            => 'Propietario y usuarios en',
         'Owner Only'
-            => '??',
+            => 'Propietario sólo',
         'Parent Group'
-            => '??',
+            => 'Parent group',
         'Parent Role'
-            => '??',
+            => 'Parent role',
         'Password'
-            => 'ContraseÃ±a',
+            => 'Contraseña',
         'Permissions'
-            => '??',
+            => 'Permisos',
         'Permitable'
-            => '??',
+            => 'Permitable',
         'Please use FireFox, Chrome, or Internet Explorer.'
             => 'Por favor, use Firefox, Chrome, o Internet Explorer.',
         'Policies'
-            => '??',
+            => 'Reglas',
         'Policies Saved Successfully.'
-            => '??',
+            => 'Reglas guardado correctamente',
         'Popup list page size'
-            => '??',
+            => 'Número de elementos en la lista popup',
         'Postal Code'
-            => '??',
+            => 'Código postal',
         'Primary Address'
-            => '??',
+            => 'Dirección principal',
         'Primary Email'
-            => '??',
+            => 'Email principal',
         'Rate to'
-            => '??',
+            => 'Tipo de cambio a',
         'Rate To Base'
-            => '??',
+            => 'Tipo de cambio base',
         'Read permissions rebuild complete.'
-            => '??',
+            => 'Reconstrucción de permisis lectura completo',
         'Recently Viewed'
-            => '??',
+            => 'Visto recientemente',
         'Remember me next time'
-            => '??',
+            => 'Recordarme la próxima vez',
         'Return to Admin Menu'
-            => '??',
+            => 'Volver al menú de administración',
         'Rights'
-            => '??',
+            => 'Derechos',
         'Rights Saved Successfully.'
-            => '??',
+            => 'Derechos guardado correctamente',
         'Role'
-            => '??',
+            => 'Role',
         'Roles'
-            => '??',
+            => 'Roles',
         'Save Changes'
-            => '??',
+            => 'Guardar cambios',
         'Search'
             => 'Buscar',
         'Search by name, phone, or e-mail'
-            => '??',
+            => 'Buscar por nombre, teléfono, o e-mail',
         'Secondary Address'
-            => '??',
+            => 'Dirección secundaria',
         'Secondary Email'
-            => '??',
+            => 'Email secundaria',
         'Securable Item'
-            => '??',
+            => 'Artículo asegurable',
         'Security'
-            => '??',
+            => 'seguridad',
         'Select a Parent Group'
-            => '??',
+            => 'Seleccione un grupo de padres',
         'Select a Parent Role'
-            => '??',
+            => 'Seleccione un role de padres',
         'Select a Role'
-            => '??',
+            => 'Seleccione un role',
         'Shipping Address'
-            => '??',
+            => 'Dirección de envío',
         'Size'
-            => '??',
+            => 'Tamaño',
         'skipped because you do not have sufficient permissions.'
-            => '??',
+            => 'Omitido porque no tiene los permisos necesarios',
         'Sorry! Your browser is not supported.'
-            => 'Lo sentimos! Su navegador no es compatible.',
+            => '¡Lo siento! Su navegador no es compatible',
         'Source'
-            => '??',
+            => 'Fuente',
         'Stage'
-            => '??',
+            => 'Etapa',
         'State'
-            => '??',
+            => 'Estado',
         'Street 1'
-            => '??',
+            => 'Calle 1',
         'Street 2'
-            => '??',
+            => 'Calle 2',
         'Sublist page size'
-            => '??',
+            => 'Número de elementos de la sublista',
         'Successfully updated'
-            => '??',
+            => 'Actualizado correctamente',
         'Super Administrators'
             => 'Superadministradores',
         'The currency rate web service says this currency code is invalid even though zurmo says it is valid. The rate could not be automatically updated.'
-            => '??',
+            => 'El servicio web para los tipos de cambio indica que este código de divisa no es válida, aunque zurmo indica que es válida. El tipo de cambio no puede ser actualizado automáticamente',
         'The currency rate web service was unavailable. The rate could not be automatically updated.'
-            => '??',
+            => 'El servicio web para los tipos de cambio no estaba disponible. El tipo de cambio no puede ser actualizado automáticamente.',
         'The currency was not removed because it is in use.'
-            => '??',
+            => 'La moneda no se ha eliminado porque está en uso.',
         'There are no recently viewed items.'
-            => '??',
+            => 'No hay elementons recientemente vistos',
         'There are required fields missing from the following layout: {view}.  Please contact your administrator.'
-            => '??',
+            => 'Campos obligatorios faltan en la siguiente diseño: {vista}. Por favor, contacte con su administrador.',
         'There was an error with the web service.'
-            => '??',
+            => 'Hubo un error con el servicio web',
         'This action is only available in debug mode.'
-            => '??',
+            => 'Esta acción sólo está disponible en modo debug',
         'This name is reserved. Please pick a different name.'
-            => '??',
+            => 'Este nombre está reservado. Por favor, elija un nombre distinto.',
         'Time zone'
-            => '??',
+            => 'Zona horaria',
         'Title'
-            => '??',
+            => 'Título',
         'to continue to the home page. If the error persists please contact your administrator.'
-            => 'para continuar a la pÃ¡gina de inicio. Si el error persiste, por favor haga una cosa u otra.',
+            => 'para continuar a la página de inicio. Si el error persiste, por favor comuníquese con el administrador.',
         'Type a currency code or name to search.'
-            => '??',
+            => 'Escriba un código de divisa o nombre para buscar',
         'User Membership'
-            => '??',
+            => 'Miembros del grupo',
         'User Membership Saved Successfully.'
-            => '??',
+            => 'Miembros del grupo guardado con éxito',
         'Username'
-            => 'Usario',
+            => 'Usuario',
         'Value'
-            => '??',
+            => 'Valor',
         'Website'
-            => '??',
+            => 'Sitio web',
         'Welcome'
             => 'Bienvenidos',
         'What is this?'
-            => '??',
+            => '¿Qué es esto?',
         'Who can read and write'
-            => '??',
+            => 'Permiso de lectura / escritura',
         'You have tried to access a page you do not have access to.'
-            => '??',
+            => 'Ha intentado acceder a una página que no tienen acceso.',
         'You must have at least one active currency.'
-            => '??',
+            => 'Debe tener al menos una divisa activa.',
         'Your Profile'
             => 'Su Perfil',
         'Your RedBean version is currentVersion and it should be acceptableVersion.'
-            => '??',
+            => 'Su versión de RedBean es currentVersion y debe ser acceptableVersion.',
         'Your Yii version is currentVersion and it should be acceptableVersion.'
-            => '??',
+            => 'Su versión de Yii es currentVersion y debe ser acceptableVersion',
         'ZurmoCRM'
             => 'ZurmoCRM',
     );
