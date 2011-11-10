ae5fa20eaa2360175207a69646d8712a64eba875
digimagic <none@none>
2011-11-10 15:08:15 -0600

    German translation for framework files

--- app/protected/extensions/zurmoinc/framework/messages/de/Default.php
+++ app/protected/extensions/zurmoinc/framework/messages/de/Default.php
@@ -30,239 +30,240 @@
     // DON'T MAKE A MESS!
     return array(
         '(None)'
-            => '??',
+            => '(keine)',
         'Add File'
-            => '??',
+            => 'Datei hinzufügen',
         'Add Files'
-            => '??',
+            => 'Dateien hinzufügen',
         'Add Item'
-            => '??',
+            => 'Element hinzufügen',
         'Advanced Search'
-            => '??',
+            => 'Erweiterte Suche',
         'After'
-            => '??',
+            => 'Nach',
         'All'
-            => '??',
+            => 'Alle',
         'Basic Search'
-            => '??',
+            => 'Einfache Suche',
         'Before'
-            => '??',
+            => 'Vor',
         'by'
-            => '??',
+            => 'Von',
         'Cancel'
-            => '??',
+            => 'Abbrechen',
         'Cell Settings'
-            => '??',
+            => 'Zelle Einstellungen',
         'Celsius'
-            => '??',
+            => 'Celsius',
         'Change Log'
-            => '??',
+            => 'Änderungsprotokoll',
         'Changed'
-            => '??',
+            => 'Geändert',
         'Clear Search'
-            => '??',
+            => 'Suche löschen',
         'Create'
-            => '??',
+            => 'Erstellen',
         'Created By User'
-            => '??',
+            => 'Erstellt von User',
         'Created Date and User'
-            => '??',
+            => 'Erstellt Datum und User',
         'Created Date Time'
-            => '??',
+            => 'Erstellt Datum Uhrzeit',
         'Created On'
-            => '??',
+            => 'Erstellt am',
         'CustomManagement class needs to be extended.'
-            => '??',
+            => 'CustomManagement Klasse muss erweitert werden',
         'Date'
-            => '??',
+            => 'Datum',
         'Date Time'
-            => '??',
+            => 'Datum Uhrzeit',
         'Default Value'
-            => '??',
+            => 'Ausgangswert',
         'Delete'
-            => '??',
+            => 'Löschen',
         'Detail View Only'
-            => '??',
+            => 'Nur Detailansicht',
         'Details'
-            => '??',
+            => 'Details',
         'Done'
-            => '??',
+            => 'Fertig',
         'Edit'
-            => '??',
+            => 'Editieren',
         'Edit Portlet'
-            => '??',
+            => 'Portlet bearbeiten',
         'Error'
-            => '??',
+            => 'Fehler',
         'Fahrenheit'
-            => '??',
+            => 'Fahrenheit',
         'Filtered Lists'
-            => '??',
+            => 'Gefilterte Listen',
         'firstDateTime must occur after secondDateTime'
-            => '??',
+            => 'firstDateTime muss nach secondDateTime kommen',
         'firstDateTime must occur before secondDateTime'
-            => '??',
+            => 'firstDateTime muss vor secondDateTime kommen',
         'Float'
-            => '??',
+            => 'Float',
         'from'
-            => '??',
+            => 'von',
         'Full Name'
-            => '??',
+            => 'voller Name',
         'Go to Details'
-            => '??',
+            => 'Zur Detailansicht',
         'Hour'
-            => '??',
+            => 'Stunde',
         'Info'
-            => '??',
+            => 'Info',
         'Inline Edit View'
-            => '??',
+            => 'Inline Edit Ansicht',
         'Integer'
-            => '??',
+            => 'Ganzzahl',
         'Invalid'
-            => '??',
+            => 'Ungültig',
         'Invalid currency code'
-            => '??',
+            => 'Ungültige Währungscode',
         'Last 7 Days'
-            => '??',
+            => 'Letzte 7 Tage',
         'Last Modified'
-            => '??',
+            => 'Letzte Änderung',
         'Last Modified Date and User'
-            => '??',
+            => 'Letzte Änderung Datum und User ',
         'List'
-            => '??',
+            => 'Liste',
         'Loading'
-            => '??',
+            => 'Loading',
         'Mass Update'
-            => '??',
+            => 'Massenaktualisierung',
         'Max upload size: {maxSize}'
-            => '??',
+            => 'Max Upload-Größe: {maxSize}',
         'Minute'
-            => '??',
+            => 'Minute',
         'Model Class Name'
-            => '??',
+            => 'Modell Klassenname',
         'Model Id'
-            => '??',
+            => 'Modell Id',
         'Modified By User'
-            => '??',
+            => 'Geändert von user',
         'Modified Date Time'
-            => '??',
+            => 'Geändert Datum Uhrzeit',
         'Module Name'
-            => '??',
+            => 'Modulname',
         'More Details'
-            => '??',
+            => 'Weitere Details',
         'More Options'
-            => '??',
+            => 'Weitere Optionen',
         'My List View'
-            => '??',
+            => 'Meine Liste Ansicht',
         'Name'
-            => '??',
+            => 'Name',
         'Next 7 Days'
-            => '??',
+            => 'Nächste 7 Tage',
         'No'
-            => '??',
+            => 'Nein',
         'No import processes found.'
-            => '??',
+            => 'Kein Import-Prozesse gefunden',
         'None'
-            => '??',
+            => 'Keiner',
         'Now'
-            => '??',
+            => 'Jetzt',
         'of'
-            => '??',
+            => 'von',
         'Opted Out'
-            => '??',
+            => 'Opted Out',
         'or'
-            => '??',
+            => 'oder',
         'Panel'
-            => '??',
+            => 'Panel',
         'Panel Settings'
-            => '??',
+            => 'Panel Einstellungen',
         'Panel Title'
-            => '??',
+            => 'Panel-Titel',
         'Perform Action'
-            => '??',
+            => 'Aktion ausführen',
         'Radio Drop Down'
-            => '??',
+            => 'Radio Drop Down',
         'record'
-            => '??',
+            => 'Datensatz',
         'records'
-            => '??',
+            => 'Datensätze',
         'Remove'
-            => '??',
+            => 'Entfernen',
         'Return to List'
-            => '??',
+            => 'Zurück zur Liste',
         'Row'
-            => '??',
+            => 'Reihe',
         'Save'
-            => '??',
+            => 'Speichern',
         'Search'
-            => '??',
+            => 'Suchen',
         'Second'
-            => '??',
+            => 'Zweite',
         'Select'
-            => '??',
+            => 'Wählen',
         'selected for updating.'
-            => '??',
+            => 'Ausgewählt zur Aktualisierung',
         'Serialized Data'
-            => '??',
+            => 'Serialisierte Daten',
         'Some values cannot be removed because they are currently in use. ' .
         'Try changing the records that use them first.'
-            => '??',
+            => 'Einige Werte können nicht entfernt werden, da sie gerade in Gebrauch sind'.
+               'Ändern Sie die Datensätze, die sie zum ersten Mal verwenden',
         'Start'
-            => '??',
+            => 'Anfang',
         'Tab'
-            => '??',
+            => 'Tab',
         'The file did not exist'
-            => '??',
+            => 'Diese Datei existierte nicht',
         'There was an error uploading the file.'
-            => '??',
+            => 'Es gab einen Fehler beim Upload der Datei.',
         'There was an error with the web service.'
-            => '??',
+            => 'Es gab einen Fehler mit dem Web-Service.',
         'This module doesn\'t have anything to configure.'
-            => '??',
+            => 'Dieses Modul hat nichts zu konfigurieren.',
         'This pick-list is used by more than one module.'
-            => '??',
+            => 'Diese Pick-Liste ist nach mehr als einem Modul verwendet.',
         'This portlet will be removed, ok?'
-            => '??',
+            => 'Dieses Portlet werden entfernt, ok?',
         'Time'
-            => '??',
+            => 'Uhrzeit',
         'Title/First/LastName'
-            => '??',
+            => 'Titel/Vorname/Nachname',
         'to'
-            => '??',
+            => 'zu',
         'Today'
-            => '??',
+            => 'Heute',
         'Tomorrow'
-            => '??',
+            => 'Morgen',
         'total'
-            => '??',
+            => 'gesamt',
         'Type'
-            => '??',
+            => 'Typ',
         'Unknown'
-            => '??',
+            => 'unbekannt',
         'Update Selected'
-            => '??',
+            => 'Aktualisierung ausgewählt',
         'updated successfully.'
-            => '??',
+            => 'erfolgreich aktualisiert.',
         'Updating'
-            => '??',
+            => 'Aktualisierung',
         'User'
-            => '??',
+            => 'User',
         'Users'
-            => '??',
+            => 'Users',
         'Yes'
-            => '??',
+            => 'Ja',
         'Yesterday'
-            => '??',
+            => 'Gestern',
         'You must enter a value'
-            => '??',
+            => 'Sie müssen einen Wert eingeben',
         'You must select at least one field to modify.'
-            => '??',
+            => 'Wählen Sie mindestens ein Feld zu ändern.',
         'You must select at least one record'
-            => '??',
+            => 'Wählen Sie mindestens einen Datensatz',
         'You must specify a value.'
-            => '??',
+            => 'Geben Sie einen Wert',
         '{attribute} "{value}" is already in use.'
-            => '??',
+            => '{attribute} "{value}" ist bereits im Einsatz.',
         '{attribute} is too precise (maximum decimal places is {precision}).'
-            => '??',
+            => '{attribute} ist zu genau (maximum decimal places is {precision})',
     );
 ?>
