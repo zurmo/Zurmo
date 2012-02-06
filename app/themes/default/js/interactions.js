84cf3bd151169384d5e59909fa8a39fb1264f99e
Amit_Ashckenazi <obviocity@gmail.com>
2012-02-06 19:03:16 -0300

    Edit Note Portlet. Cleaned Main Nav JS.
    --HG--
    branch : newuserinterface

--- app/themes/default/js/interactions.js
+++ app/themes/default/js/interactions.js
@@ -1,23 +1,18 @@
 $(window).ready(function(){
 	
-	//test - resizing left bar.
-	//$( "#MenuView" ).resizable({ alsoResize: '#RecentlyViewedView' });
-
 	//main menu flyouts
-
 	$( '#MenuView .parent' ).hover(
 		function(){
 			if ( $(this).find('ul') ){
-				$(this).find('ul').addClass('active-submenu');
 				$(this).find('ul').stop(true, true).fadeIn(100);
 			}
 		}, 
 		function(){
 			if ( $(this).find('ul') ){
-				$(this).find('ul').removeClass('active-submenu');
 				$(this).find('ul').stop(true, true).fadeOut(250);
 			}
 		}
 	);
+	
 
 });
