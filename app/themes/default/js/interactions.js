14aec3ec84495f5b06c18190c4c9d50ab0cc96f1
Amit_Ashckenazi <obviocity@gmail.com>
2012-02-13 21:40:29 -0300

    Fixes to account view
    --HG--
    branch : newuserinterface

--- app/themes/default/js/interactions.js
+++ app/themes/default/js/interactions.js
@@ -14,19 +14,4 @@ $(window).ready(function(){
 		}
 	);
 	
-	
-	/*
-	$( '#HeaderLinksView .parent' ).hover(
-		function(){
-			if ( $(this).find('ul') ){
-				$(this).find('ul').stop(true, true).fadeIn(100);
-			}
-		}, 
-		function(){
-			if ( $(this).find('ul') ){
-				$(this).find('ul').stop(true, true).fadeOut(250);
-			}
-		}
-	);
-	*/
 });
