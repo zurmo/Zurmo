3ebbcac4ce71eaef27f92ee4b2e9518159f3b189
Amit_Ashckenazi <obviocity@gmail.com>
2012-02-15 15:36:08 -0300

    Styling for options menu in selected account view. generalized jQuery functions for drop downs like mbmenu.
    --HG--
    branch : newuserinterface

--- app/themes/default/js/interactions.js
+++ app/themes/default/js/interactions.js
@@ -1,7 +1,8 @@
 $(window).ready(function(){
 	
 	//main menu flyouts or mbmenu releacment
-	$( '#MenuView .parent, #HeaderLinksView .parent, #ShortcutsMenu .parent' ).hover(
+	//$( '#MenuView .parent, #HeaderLinksView .parent, #ShortcutsMenu .parent' ).hover(
+	$( '.nav > .parent' ).hover(
 		function(){
 			if ( $(this).find('ul') ){
 				$(this).find('ul').stop(true, true).fadeIn(100);
