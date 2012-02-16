917b37ba4b8dd929c8c99004a48e823ee8c85b43
zurmo <none@none>
2012-02-16 00:30:06 -0500

    License update

--- app/protected/modules/zurmo/utils/CalculatedNumberUtil.php
+++ app/protected/modules/zurmo/utils/CalculatedNumberUtil.php
@@ -1,7 +1,7 @@
 <?php
     /*********************************************************************************
      * Zurmo is a customer relationship management program developed by
-     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
      *
      * Zurmo is free software; you can redistribute it and/or modify it under
      * the terms of the GNU General Public License version 3 as published by the
@@ -22,7 +22,7 @@
      *
      * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
      * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
-     *********************************************************************************/
+     ********************************************************************************/
 
     /**
      * Helper class to work with calculated numbers.
