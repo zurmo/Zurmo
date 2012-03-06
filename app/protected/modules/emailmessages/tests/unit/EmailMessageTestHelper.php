1de233a3564498d8701747e9d16fbcab41c62f35
digimagic <none@none>
2012-03-06 12:17:07 -0600

    Adding ZurmoSwiftMailer handling.
    --HG--
    branch : emailMessages

---
+++ app/protected/modules/emailmessages/tests/unit/EmailMessageTestHelper.php
@@ -0,0 +1,65 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    class EmailMessageTestHelper
+    {
+        public static function createDraftSystemEmail($subject)
+        {
+            $emailMessage = new EmailMessage();
+            $emailMessage->owner   = Yii::app()->emailHelper->getUserToSendNotificationsAs();
+            $emailMessage->subject = $subject;
+
+            //Set sender, and recipient, and content
+            $emailContent              = new EmailMessageContent();
+            $emailContent->textContent = 'My First Message';
+            $emailContent->htmlContent = 'Some fake HTML content';
+            $emailMessage->content     = $emailContent;
+
+            //Sending from the system, does not have a 'person'.
+            $sender                    = new EmailMessageSender();
+            $sender->fromAddress       = 'system@somewhere.com';
+            $sender->fromName          = 'Zurmo System';
+            $emailMessage->sender      = $sender;
+
+            //Recipient is billy.
+            $recipient                 = new EmailMessageRecipient();
+            $recipient->toAddress      = 'billy@fakeemail.com';
+            $recipient->toName         = 'Billy James';
+            $recipient->type           = EmailMessageRecipient::TYPE_TO;
+            $emailMessage->recipients->add($recipient);
+
+            $box                  = EmailBox::resolveAndGetByName(EmailBox::NOTIFICATIONS_NAME);
+            $emailMessage->folder = EmailFolder::getByBoxAndType($box, EmailFolder::TYPE_DRAFT);
+
+            //Save, at this point the email should be in the draft folder
+            $saved = $emailMessage->save();
+            if(!$saved)
+            {
+                throw new NotSupportedException($message, $code, $previous);
+            }
+            return $emailMessage;
+        }
+    }
