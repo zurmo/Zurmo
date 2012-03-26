411bb94c781ca2a4cee6b6b7b56dc84952ec7598
Amit_Ashckenazi <obviocity@gmail.com>
2012-03-26 15:16:00 -0300

    checkboxes in language list in designer
    --HG--
    branch : newuserinterface

--- app/protected/modules/designer/views/ModuleEditableMetadataCollectionView.php
+++ app/protected/modules/designer/views/ModuleEditableMetadataCollectionView.php
@@ -70,9 +70,11 @@
                 $content .= '</li>';
             }
             $content .= '</ul>';
-            $content .= '<div class="view-toolbar-container clearfix"><div class="form-toolbar">';
-            $content .= $this->renderActionElementBar(false);
-            $content .= '</div></div>';
+            /*Do we need those if anyhow we dont render anything to page? in terms of css, teh empty container has a white border
+			 * I want to NOT have..*/
+            //$content .= '<div class="view-toolbar-container clearfix"><div class="form-toolbar">';
+            //$content .= $this->renderActionElementBar(false);
+            //$content .= '</div></div>';
             $content .= '</div>';
             return $content;
         }
