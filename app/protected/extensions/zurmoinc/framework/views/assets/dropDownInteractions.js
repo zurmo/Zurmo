cc3a30b891596b8ff71407cb7c0fb5369964ba30
digimagic <none@none>
2012-04-02 17:06:20 -0500

    Fixing dropkick to only initiate on none multiple-selects
    --HG--
    branch : newuserinterface

--- app/protected/extensions/zurmoinc/framework/views/assets/dropDownInteractions.js
+++ app/protected/extensions/zurmoinc/framework/views/assets/dropDownInteractions.js
@@ -1,6 +1,9 @@
 $(window).ready(function(){
     /*Dropdowns - Dropkick*/
     $('select:not(.ignore-style)').each(function(){
+        if($(this).attr('multiple') != 'multiple')
+        {
         $(this).dropkick();
+        }
     });
 });
